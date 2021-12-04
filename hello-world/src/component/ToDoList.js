fd62069f073a0bc15806968a80ffc12731825301
Ibrahim Oliyide <oliyideibrahim123@gmail.com>
2021-12-04 19:54:57 +0100

    remove the un-needed styling for the style sheet

--- hello-world/src/component/ToDoList.js
+++ hello-world/src/component/ToDoList.js
@@ -1,22 +1,12 @@
 import React, { useEffect, useState } from 'react';
 import {MdDelete} from 'react-icons/md';
 
-function ToDoList() {
-    const toDo = {
-        task: " Send the monthly transactions to Customers",
-        details: "Sending the monthly email to all the classic customers of my organization contain details about what they purchased and how much they where charged for it.",
-        date: "2021-07-30",
-        status: "Open",
-        priority: "Very Low",
-        comment: "Copy the executives while sending the email"
-    }
-    const [actionLog, setAction]= useState([]);
-    const [task,setTask] = useState(toDo.task);
-    const [details,setDetails] = useState(toDo.details);
-    const [date,setDate] = useState(toDo.date);
-    const [status,setStatus] = useState(toDo.status);
-    const [priority,setPriority]= useState(toDo.priority);
-    const [comment,setComment]= useState(toDo.comment);
+function ToDoList({actionLog, setAction}) {
+    const [task,setTask] = useState('');
+    const [details,setDetails] = useState('');
+    const [date,setDate] = useState('');
+    const [priority,setPriority]= useState('');
+    const [comment,setComment]= useState('');
     useEffect(() => {
         setAction([
             ...actionLog,
@@ -24,20 +14,19 @@ function ToDoList() {
                task: "Send the monthly transactions to Customers",
                details: "Sending the monthly email to all the classic customers of my organization contain details about what they purchased and how much they where charged for it.",
                date: "2021-07-30",
-               status: "Open",
                priority: "High",
                comment: "no comments is available",
             }
-        ])  
+        ]) 
         },[])
 
     const changeTask = (e) => {setTask(e.target.value)}
     const changeDetails = (e) => {setDetails(e.target.value)}
     const changeDate = (e) => {setDate(e.target.value)}
-    const changeStatus = (e) => {setStatus(e.target.value)}
     const changePriority = (e) => {setPriority(e.target.value)}
     const changeComment = (e) => {setComment(e.target.value)}
-    const addTask = () => {
+    const addTask = (e) => {
+        e.preventDefault();
         if (task && details && date) {
         setAction([
             ...actionLog,
@@ -45,7 +34,6 @@ function ToDoList() {
                 task:task,
                 details:details,
                 date:date,
-                status:status,
                 priority:priority,
                 comment:comment,
             }
@@ -61,6 +49,8 @@ function ToDoList() {
         }
         
     }
+    
+    
     return (
         <>
         <div className="todo" >
@@ -77,16 +67,6 @@ function ToDoList() {
         <p className="task-box">
             <label className="task-label">Date:</label>
             <input className="task-input" type="date" required onChange={(e) => changeDate(e)}  value={date} tabIndex="3"></input>
-        </p>
-        <p className="task-box">
-            <label className="task-label" htmlFor="status">Status:</label>
-            <select className="task-input" type="text"  name="status" id="status" value={status} onChange={(e) => changeStatus(e)} required  tabIndex="4">
-                <option value="Open">Open</option>
-                <option value="Closed">Closed</option>
-                <option value="Delay">Delay</option>
-                <option value="Over Due">Over Due</option>
-                <option value="Cancelled">Cancelled</option>
-            </select>
         </p>
         <p className="task-box">
             <label className="task-label" htmlFor="priority">Priority:</label>
@@ -113,7 +93,6 @@ function ToDoList() {
                                 <th>Task</th>
                                 <th>Details</th>
                                 <th>Date</th>
-                                <th id="stat" >Status</th>
                                 <th id="prior" >Priority</th>
                                 <th>Comment</th>
                               </tr>
@@ -132,7 +111,6 @@ function ToDoList() {
                                 )} id="delete" ><MdDelete/></button>{action.task}</td>
                                 <td>{action.details}</td>
                                 <td>{action.date} </td>
-                                <td>{action.status} </td>
                                 <td>{action.priority}</td>
                                 <td>{action.comment}</td>
                                 
